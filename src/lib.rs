21324dca0d7905279186af3de0f74ac69e04f6f3
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-05-09 10:21:58 +0300

    tx schema

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 pub mod error;
 //pub mod types;
 //pub mod get_blocks;
-//pub mod parquet_writer;
+pub mod parquet_writer;
 
 pub use error::{Error, Result};
 pub mod retry;
--- src/parquet_writer.rs
+++ src/parquet_writer.rs
@@ -1,58 +1,178 @@
-use tokio::sync::mpsc;
 use crate::types::BlockData;
-use std::path::{Path, PathBuf};
+use parquet::basic::Repetition;
+use parquet::basic::Type as BasicType;
+use parquet::schema::types::{Type, TypePtr};
 use std::fs::File;
+use std::path::{Path, PathBuf};
+use std::sync::Arc;
+use tokio::sync::mpsc;
 
 pub struct ParquetWriter {
-	tx: mpsc::UnboundedSender<BlockData>,
+    tx: mpsc::UnboundedSender<BlockData>,
 }
 
 impl ParquetWriter {
-	pub fn new<P: AsRef<Path>, S: Into<String>>(name: S, path: P) -> Result<Self> {
-		let (tx, rx) = mpsc::unbounded_channel();
+    pub fn new<P: AsRef<Path>, S: Into<String>>(name: S, path: P) -> Result<Self> {
+        let (tx, rx) = mpsc::unbounded_channel();
 
-		let task = WriteTask::new(name.into(), Path::from(path.as_ref()))?;
+        let task = WriteTask::new(name.into(), Path::from(path.as_ref()))?;
 
-		tokio::spawn_blocking(|| async move { task.run(); });
+        tokio::spawn_blocking(|| async move {
+            task.run();
+        });
 
-		Self {
-			tx,
-		}
-	}
+        Self { tx }
+    }
 
-	pub fn write(block: BlockData) {
-		self.tx.send(block).unwrap();
-	}
+    pub fn write(block: BlockData) {
+        self.tx.send(block).unwrap();
+    }
 }
 
 struct WriteTask {
-	path: Path,
-	file: File,
-	rx: mpsc::UnboundedReceiver<BlockData>,
-	next_file_idx: usize,
-	name: String,
+    path: Path,
+    file: File,
+    rx: mpsc::UnboundedReceiver<BlockData>,
+    next_file_idx: usize,
+    name: String,
+    schema: TypePtr,
 }
 
 impl WriteTask {
-	fn new(name: String, path: Path) -> Result<Self> {
-		let mut buf = self.path.to_path_buf();
-		buf.push(format!("{}{}.patquet", &name, 0));
+    fn new(name: String, path: Path) -> Result<Self> {
+        let mut buf = self.path.to_path_buf();
+        buf.push(format!("{}{}.parquet", &name, 0));
 
-		let file = File::create(buf.as_path()).await?;
+        let file = File::create(buf.as_path()).await?;
 
-		Self {
-			path, file, file_idx: 1, name,
-		}
-	}
+        let type_ptr = Type::group_type_builder(&name).with_fields(&mut vec![
+            Arc::new(
+                Type::primitive_type_builder("hash", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::REQUIRED)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("nonce", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::REQUIRED)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("block_hash", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("block_number", BasicType::INT64),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("transaction_index", BasicType::INT64),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("from", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(20)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("to", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(20)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("value", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::REQUIRED)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("gas_price", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(32)
+                    .build()asd
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("gas", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::REQUIRED)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("input", BasicType::BYTE_ARRAY)
+                    .with_repetition(Repetition::REQUIRED)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("v", BasicType::INT64),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("r", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+               Arc::new(
+                Type::primitive_type_builder("s", BasicType::FIXED_LEN_BYTE_ARRAY),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .with_length(32)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("raw", BasicType::BYTE_ARRAY)
+                    .with_repetition(Repetition::OPTIONAL)
+                    .build()
+                    .unwrap()
+            ),
+            Arc::new(
+                Type::primitive_type_builder("transaction_type", BasicType::INT64),
+                    .with_repetition(Repetition::OPTIONAL)
+                    .build()
+                    .unwrap()
+            ),
+        ]);
 
-	async fn run(self) {
-		while let Some(block) = self.rx.recv() {
-			self.file.write
-		}
-	}
+        let type_ptr = Arc::new(type_ptr);
 
-	fn open_new_file(&self) -> Result<File> {
-		let mut buf = self.path.to_path_buf();
-		buf.push()
-	} 
+        Self {
+            path,
+            file,
+            file_idx: 1,
+            name,
+            type_ptr,
+        }
+    }
+
+    async fn run(self) {
+        while let Some(block) = self.rx.recv() {
+            self.file.write
+        }
+    }
+
+    fn open_new_file(&self) -> Result<File> {
+        let mut buf = self.path.to_path_buf();
+        buf.push()
+    }
 }
