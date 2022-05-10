17248211ef61efbecfd601d4eac550576705bd5a
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-05-10 05:43:26 +0300

    use crossbeam

--- src/parquet_writer.rs
+++ src/parquet_writer.rs
@@ -1,45 +1,45 @@
 use crate::types::WriteToParquet;
 use crate::{Error, Result};
+use crossbeam::channel;
 use parquet::basic::Repetition;
 use parquet::basic::Type as BasicType;
 use parquet::schema::types::{Type, TypePtr};
 use std::fs::File;
 use std::path::{Path, PathBuf};
 use std::sync::Arc;
-use tokio::sync::mpsc;
 
 pub struct ParquetWriter<T: WriteToParquet> {
-    tx: mpsc::UnboundedSender<T>,
+    tx: channel::Sender<Vec<T>>,
 }
 
 impl<T: WriteToParquet> ParquetWriter<T> {
     pub fn new<P: AsRef<Path>>(path: P, schema: TypePtr) -> Result<Self> {
-        let (tx, rx) = mpsc::unbounded_channel();
+        let (tx, rx) = channel::unbounded();
 
         let task = WriteTask::new(path.as_ref(), rx, schema)?;
 
-        tokio::task::spawn_blocking(|| async move {
+        std::thread::spawn(|| async move {
             task.run().await;
         });
 
         Ok(Self { tx })
     }
 
-    pub fn write(&self, val: T) {
-        self.tx.send(val).unwrap();
+    pub fn write(&self, chunk: Vec<T>) {
+        self.tx.send(chunk).unwrap();
     }
 }
 
 struct WriteTask<T: WriteToParquet> {
     path: PathBuf,
     file: File,
-    rx: mpsc::UnboundedReceiver<T>,
+    rx: channel::Receiver<Vec<T>>,
     next_file_idx: usize,
     schema: TypePtr,
 }
 
 impl<T: WriteToParquet> WriteTask<T> {
-    fn new(path: &Path, rx: mpsc::UnboundedReceiver<T>, schema: TypePtr) -> Result<Self> {
+    fn new(path: &Path, rx: channel::Receiver<Vec<T>>, schema: TypePtr) -> Result<Self> {
         let mut path = path.to_path_buf();
         path.push(format!("{}{}.parquet", schema.name(), 0));
         let file = File::create(path.as_path()).map_err(Error::CreateParquetFile)?;
@@ -55,8 +55,6 @@ impl<T: WriteToParquet> WriteTask<T> {
     }
 
     async fn run(mut self) {
-        while let Some(block) = self.rx.recv().await {
-            
-        }
+        while let Ok(chunk) = self.rx.recv() {}
     }
 }
--- src/types.rs
+++ src/types.rs
@@ -1,9 +1,24 @@
+use crate::{Error, Result};
 use web3::types::{Block, Log, Transaction, H256};
 
-pub trait WriteToParquet: Send + Sync + std::fmt::Debug + 'static {}
+pub trait WriteToParquet: Send + Sync + std::fmt::Debug + 'static + std::marker::Sized {
+    fn write_to_row_group(chunk: Vec<Self>) -> Result<()>;
+}
 
-impl WriteToParquet for &[Block<H256>] {}
+impl WriteToParquet for Block<H256> {
+    fn write_to_row_group(chunk: Vec<Self>) -> Result<()> {
+        unimplemented!();
+    }
+}
 
-impl WriteToParquet for &[Transaction] {}
+impl WriteToParquet for Transaction {
+    fn write_to_row_group(chunk: Vec<Self>) -> Result<()> {
+        unimplemented!();
+    }
+}
 
-impl WriteToParquet for &[Log] {}
+impl WriteToParquet for Log {
+    fn write_to_row_group(chunk: Vec<Self>) -> Result<()> {
+        unimplemented!();
+    }
+}
