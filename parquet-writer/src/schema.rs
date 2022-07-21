90f5ebd6db0e214aae5b7c7b30cb45501cc96f1a
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-07-21 10:39:25 +0300

    add default impls on columnar structs

--- parquet-writer/src/schema.rs
+++ parquet-writer/src/schema.rs
@@ -1,5 +1,8 @@
 use crate::{Error, Result};
-use arrow2::array::{Array, MutableArray, MutableBooleanArray, MutableUtf8Array, UInt64Vec};
+use arrow2::array::{
+    Array, MutableArray, MutableBinaryArray as ArrowMutableBinaryArray, MutableBooleanArray,
+    MutableFixedSizeBinaryArray, MutableUtf8Array, UInt64Vec,
+};
 use arrow2::chunk::Chunk as ArrowChunk;
 use arrow2::compute::sort::{lexsort_to_indices, sort_to_indices, SortColumn, SortOptions};
 use arrow2::compute::take::take as arrow_take;
@@ -8,11 +11,14 @@ use arrow2::error::ArrowError;
 use arrow2::io::parquet::write::{
     CompressionOptions, Encoding, RowGroupIterator, Version, WriteOptions,
 };
+use eth_archive_core::types::{Block, Log, Transaction};
 use rayon::iter::{IntoParallelIterator, ParallelIterator};
 use serde::{Deserialize, Serialize};
 use std::result::Result as StdResult;
 
 type Chunk = ArrowChunk<Box<dyn Array>>;
+
+type MutableBinaryArray = ArrowMutableBinaryArray<i64>;
 
 fn bytes32() -> DataType {
     DataType::FixedSizeBinary(32)
@@ -92,7 +98,19 @@ fn options() -> WriteOptions {
     }
 }
 
-#[derive(Debug, Default)]
+fn bytes32_arr() -> MutableFixedSizeBinaryArray {
+    MutableFixedSizeBinaryArray::new(32)
+}
+
+fn bloom_filter_arr() -> MutableFixedSizeBinaryArray {
+    MutableFixedSizeBinaryArray::new(256)
+}
+
+fn address_arr() -> MutableFixedSizeBinaryArray {
+    MutableFixedSizeBinaryArray::new(20)
+}
+
+#[derive(Debug)]
 pub struct Blocks {
     pub number: UInt64Vec,
     pub hash: MutableFixedSizeBinaryArray,
@@ -110,8 +128,33 @@ pub struct Blocks {
     pub size: UInt64Vec,
     pub gas_limit: UInt64Vec,
     pub gas_used: UInt64Vec,
-    pub timestamp:UInt64Vec,
+    pub timestamp: UInt64Vec,
     pub len: usize,
+}
+
+impl Default for Blocks {
+    fn default() -> Self {
+        Self {
+            number: Default::default(),
+            hash: bytes32_arr(),
+            parent_hash: bytes32_arr(),
+            nonce: Default::default(),
+            sha3_uncles: bytes32_arr(),
+            logs_bloom: bloom_filter_arr(),
+            transactions_root: bytes32_arr(),
+            state_root: bytes32_arr(),
+            receipts_root: bytes32_arr(),
+            miner: address_arr(),
+            difficulty: Default::default(),
+            total_difficulty: Default::default(),
+            extra_data: Default::default(),
+            size: Default::default(),
+            gas_limit: Default::default(),
+            gas_used: Default::default(),
+            timestamp: Default::default(),
+            len: 0,
+        }
+    }
 }
 
 type RowGroups = RowGroupIterator<Box<dyn Array>, std::vec::IntoIter<StdResult<Chunk, ArrowError>>>;
@@ -209,7 +252,7 @@ impl IntoRowGroups for Blocks {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Transactions {
     pub block_hash: MutableFixedSizeBinaryArray,
     pub block_number: UInt64Vec,
@@ -226,6 +269,28 @@ pub struct Transactions {
     pub r: MutableBinaryArray,
     pub s: MutableBinaryArray,
     pub len: usize,
+}
+
+impl Default for Transactions {
+    fn default() -> Transactions {
+        Self {
+            block_hash: bytes32_arr(),
+            block_number: Default::default(),
+            from: bytes32_arr(),
+            gas: Default::default(),
+            gas_price: Default::default(),
+            hash: bytes32_arr(),
+            input: Default::default(),
+            nonce: Default::default(),
+            to: bytes32_arr(),
+            transaction_index: Default::default(),
+            value: Default::default(),
+            v: Default::default(),
+            r: Default::default(),
+            s: Default::default(),
+            len: 0,
+        }
+    }
 }
 
 impl IntoRowGroups for Transactions {
@@ -326,7 +391,7 @@ impl IntoRowGroups for Transactions {
     }
 }
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 pub struct Logs {
     pub address: MutableFixedSizeBinaryArray,
     pub block_hash: MutableFixedSizeBinaryArray,
@@ -341,6 +406,26 @@ pub struct Logs {
     pub transaction_hash: MutableFixedSizeBinaryArray,
     pub transaction_index: UInt64Vec,
     pub len: usize,
+}
+
+impl Default for Logs {
+    fn default() -> Self {
+        Self {
+            address: bytes32_arr(),
+            block_hash: bytes32_arr(),
+            block_number: Default::default(),
+            data: Default::default(),
+            log_index: Default::default(),
+            removed: Default::default(),
+            topic0: bytes32_arr(),
+            topic1: bytes32_arr(),
+            topic2: bytes32_arr(),
+            topic3: bytes32_arr(),
+            transaction_hash: bytes32_arr(),
+            transaction_index: Default::default(),
+            len: 0,
+        }
+    }
 }
 
 impl IntoRowGroups for Logs {
