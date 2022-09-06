b81a5b73154d12bb9599c476e1b7297d9a75a2a3
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-09-06 06:43:02 +0300

    add http timeouts when downloading data

--- ingester/src/ingester.rs
+++ ingester/src/ingester.rs
@@ -33,8 +33,7 @@ impl Ingester {
             .map_err(|e| Error::CreateDbHandle(Box::new(e)))?;
         let db = Arc::new(db);
 
-        let eth_client =
-            EthClient::new(config.ingest.eth_rpc_url.clone()).map_err(Error::CreateEthClient)?;
+        let eth_client = EthClient::new(&config.ingest).map_err(Error::CreateEthClient)?;
         let eth_client = Arc::new(eth_client);
 
         let retry = Retry::new(config.retry);
