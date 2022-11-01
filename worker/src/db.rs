9ed8e34861625e2df13bbf266dc848a3f58a9d8b
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-11-01 03:06:05 +0300

    delete_up_to

--- worker/src/db.rs
+++ worker/src/db.rs
@@ -1,4 +1,3 @@
-use cuckoofilter::{CuckooFilter, ExportedCuckooFilter};
 use solana_bloom::bloom::Bloom;
 use std::path::Path;
 use std::convert::TryInto;
@@ -32,13 +31,6 @@ impl DbHandle {
         Ok(Self { inner, status })
     }
 
-    pub fn get_next_parquet_folder(
-        &self,
-        from_block: u32,
-    ) -> Result<Option<DirName>> {
-        todo!()
-    }
-
     pub fn query(&self, query: MiniQuery) -> Result<QueryResult> {
         todo!()
     }
@@ -61,10 +53,13 @@ impl DbHandle {
         let addr_tx_cf = self.inner.cf_handle(cf_name::ADDR_TX).unwrap();
         let log_tx_cf = self.inner.cf_handle(cf_name::LOG_TX).unwrap();
 
+        let tail_is_zero = self.status.db_tail.load(Ordering::Relaxed) == 0;
+
         for (blocks, logs) in block_batches.into_iter().zip(log_batches.into_iter()) {
             let db_tx = self.inner.transaction();
 
             let mut db_height = self.status.db_height.load(Ordering::Relaxed);
+            let mut db_tail = std::u32::MAX;
 
             for mut block in blocks {
                 let txs = mem::take(&mut block.transactions);
@@ -79,7 +74,8 @@ impl DbHandle {
                 let val = rmp_serde::encode::to_vec(&block).map_err(Error::MsgPack)?;
                 db_tx.put_cf(block_cf, &block.number.as_be_bytes(), &val).map_err(Error::Db)?;
 
-                db_height = block.number;
+                db_height = cmp::max(db_height, block.number);
+                db_tail = cmp::min(db_tail, block.number);
             }
 
             for log in logs {
@@ -91,12 +87,69 @@ impl DbHandle {
 
             db_tx.commit().map_err(Error::Db)?;
 
+            if db_tail != std::u32::MAX && tail_is_zero {
+                self.status.db_tail.store(db_tail, Ordering::Relaxed);
+            }
+
             self.status.db_height.store(db_height, Ordering::Relaxed);
         }
     }
 
-    pub fn delete_tail(&self) -> Result<()> {
-        todo!()
+    pub fn delete_up_to(&self, to: u32) -> Result<()> {
+        let db_height = self.status.db_height.load(Ordering::Relaxed);
+
+        if to >= db_height {
+            panic!("invalid 'to' argument passed to delete_up_to: {}", to);
+        }
+
+        let block_cf = self.inner.cf_handle(cf_name::BLOCK).unwrap();
+        let tx_cf = self.inner.cf_handle(cf_name::TX).unwrap();
+        let log_cf = self.inner.cf_handle(cf_name::LOG).unwrap();
+        let addr_tx_cf = self.inner.cf_handle(cf_name::ADDR_TX).unwrap();
+        let log_tx_cf = self.inner.cf_handle(cf_name::LOG_TX).unwrap();
+
+        let db_tail = self.status.db_tail.load(Ordering::Relaxed);
+        for block_num in db_tail..to {
+            let mut addr_tx_keys = Vec::new();
+            let mut addr_log_keys = Vec::new();
+
+            for res in self.inner.prefix_iterator_cf(tx_cf, &block_num.to_be_bytes()) {
+                let (_, tx) = res.map_err(Error::Db)?;
+                let tx = rmp_serde::decode::from_read_ref(&tx).unwrap();
+
+                addr_tx_keys.push(addr_tx_key(&tx));
+            }
+
+            for res in self.inner.prefix_iterator_cf(log_cf, &block_num.to_be_bytes()) {
+                let (_, log) = res.map_err(Error::Db)?;
+                let log = rmp_serde::decode::from_read_ref(&log).unwrap();
+
+                addr_log_key.push(addr_log_key(&log));
+            }
+
+            let mut batch = rocksdb::WriteBatchWithTransaction::<false>::default();
+
+            for key in addr_tx_keys {
+                batch.delete_cf(addr_tx_cf, &key);
+            }
+            for key in addr_log_keys {
+                batch.delete_cf(addr_log_cf, &key);
+            }
+
+            let from = block_num.to_be_bytes();
+            let to = (block_num+1).to_be_bytes();
+
+            db_tx.delete_cf(block_cf, &from);
+
+            db_tx.delete_range_cf(tx_cf, &from, &to);
+            db_tx.delete_range_cf(log_cf, &from, &to);
+
+            self.inner.write(batch).map_err(Error::Db)?;
+
+            self.status.db_tail.store(block_num+1, Ordering::Relaxed);
+        }
+
+        Ok(())
     }
 
     pub fn height(&self) -> u32 {
@@ -147,9 +200,6 @@ impl DbHandle {
     }
 }
 
-pub const LOG_ADDR_FILTER: &str = "LOG_ADDR_FILTER";
-pub const TX_ADDR_FILTER: &str = "TX_ADDR_FILTER";
-
 /// Column Family Names
 mod cf_name {
     pub const BLOCK: &str = "BLOCK";
@@ -175,32 +225,6 @@ pub struct ParquetIdx {
     pub tx_addr_filter: Bloom,
 }
 
-#[derive(Serialize, Deserialize)]
-struct ExportedCuckoo {
-    values: Vec<u8>,
-    length: usize,
-}
-
-impl From<&CuckooFilter> for ExportedCuckoo {
-    fn from(filter: &CuckooFilter) -> Self {
-        let filter = filter.export();
-
-        Self {
-            values: filter.values,
-            length: filter.length,
-        }
-    }
-}
-
-impl From<ExportedCuckoo> for CuckooFilter {
-    fn from(filter: ExportedCuckoo) -> Self {
-        Self::from(ExportedCuckooFilter {
-            values: filter.values,
-            length: filter.length,
-        })
-    }
-}
-
 fn tx_key(tx: &Transaction) -> [u8; 8] {
     let mut key = [0; 8];
 
@@ -222,7 +246,7 @@ fn log_key(log: &Log) -> [u8; 8] {
 fn addr_tx_key(tx: &Transaction) -> [u8; 28] {
     let mut key = [0; 28];
 
-    (&mut key[0..20]).copy_from_slice(tx.dest.as_slice());
+    (&mut key[..20]).copy_from_slice(tx.dest.as_slice());
     (&mut key[20..24]).copy_from_slice(&tx.block_number.to_be_bytes());
     (&mut key[24..]).copy_from_slice(&tx.transaction_index.to_be_bytes());
 
@@ -232,7 +256,7 @@ fn addr_tx_key(tx: &Transaction) -> [u8; 28] {
 fn addr_log_key(tx: &Log) -> [u8; 28] {
     let mut key = [0; 28];
 
-    (&mut key[0..20]).copy_from_slice(log.address.as_slice());
+    (&mut key[..20]).copy_from_slice(log.address.as_slice());
     (&mut key[20..24]).copy_from_slice(&log.block_number.to_be_bytes());
     (&mut key[24..]).copy_from_slice(&log.log_index.to_be_bytes());
 
@@ -240,9 +264,12 @@ fn addr_log_key(tx: &Log) -> [u8; 28] {
 }
 
 fn dir_name_from_key(key: &[u8]) -> DirName {
-    let num = u64::from_be_bytes(key.try_into().unwrap());
-    let to = num as u32;
-    let from = (num >> 32) as u32;
+    let from = (&key[..4]).try_into().unwrap();
+    let from = u32::from_be_bytes(from);
+
+    let to = (&key[4..]).try_into().unwrap();
+    let to = u32::from_be_bytes(to);
+
     DirName {
         range: BlockRange { from, to },
         is_temp: false,
@@ -252,8 +279,12 @@ fn dir_name_from_key(key: &[u8]) -> DirName {
 fn key_from_dir_name(dir_name: DirName) -> [u8; 8] {
     assert_eq!(dir_name.is_temp, false);
 
-    let num = (u64::from(dir_name.range.from) << 32) | u64::from(dir_name.range.to);
-    num.to_be_bytes()
+    let mut key = [0; 8];
+
+    (&mut key[..4]).copy_from_slice(dir_name.range.from.to_be_bytes());
+    (&mut key[4..]).copy_from_slice(dir_name.range.to.to_be_bytes());
+
+    key
 }
 
 #[cfg(test)]
