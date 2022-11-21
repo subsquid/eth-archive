5ed67b2a487ecbb41f65d6f0d3737f12b10f1007
Özgür Akkurt <oezgurmakkurt@gmail.com>
2022-11-21 04:50:04 +0300

    verifier sketch

--- verifier/src/main.rs
+++ verifier/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use eth_archive_verifier::{Config, Verifier};
+
+#[tokio::main]
+async fn main() {
+    env_logger::init();
+
+    let config = Config::parse();
+
+    let verifier = match Verifier::new(config).await {
+        Ok(verifier) => verifier,
+        Err(e) => {
+            log::error!("failed to create verifier:\n{}", e);
+            return;
+        }
+    };
+
+    if let Err(e) = verifier.run().await {
+        log::error!("failed to run verifier:\n{}", e);
+    }
 }
---
+++ verifier/src/verifier.rs
@@ -0,0 +1,14 @@
+use crate::config::Config;
+use crate::{Error, Result};
+
+pub struct Verifier {}
+
+impl Verifier {
+    pub async fn new(config: Config) -> Result<Self> {
+        todo!()
+    }
+
+    pub async fn run(self) -> Result<()> {
+        todo!()
+    }
+}
