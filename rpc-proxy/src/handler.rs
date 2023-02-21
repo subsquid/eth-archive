a4fb8ba9c6d43743dcae2e203cdb4f89ca3b6eb0
Özgür Akkurt <oezgurmakkurt@gmail.com>
2023-02-21 12:27:10 +0300

    save

--- rpc-proxy/src/handler.rs
+++ rpc-proxy/src/handler.rs
@@ -6,4 +6,21 @@ impl Handler {
 	pub async fn new(config: Config, metrics: Arc<Metrics>) -> Result<Self> {
 		
 	}
+
+	pub 
+}
+
+struct Limiter {
+	time: Instant,
+	reqs: usize,
+}
+
+impl Limiter {
+	fn new() -> Limiter {
+
+	}
+
+	fn count(&mut self) -> {
+
+	}
 }
